07124f893732ee1b437eac4eb1ae4408983cf470
dima716 <dima716@yandex.ru>
2021-11-25 17:47:06 +0300

    feat(counter): add counter

---
+++ projects/angular-mailru-counter/src/lib/angular-mail-ru-counter.models.ts
@@ -0,0 +1,6 @@
+export interface AngularMailRuCounterConfiguration {
+    /** Whether to start tracking immediately. Default is `false` */
+    enabled?: boolean;
+    /** Your Mail.ru Counter ID */
+    counterId: string;
+}
